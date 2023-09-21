b3ea0766bf0003e985353ce6e9fe5ade2e59f27c
Arvind Singh Yadav <96595110+Arvind233@users.noreply.github.com>
2023-09-21 15:21:07 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+let stringNumber = 33
+
+let valueInString = String(stringNumber)
+
+console.log(valueInString);
+console.log(typeof valueInString);
