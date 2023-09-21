0cd90f6204be6af800c98bc6a90927795c94eefb
Arvind Singh Yadav <96595110+Arvind233@users.noreply.github.com>
2023-09-21 06:44:45 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 123422
+let accountEmail = "arvind@gmail.com"
+var accountPassword = "12234544"
+let accountCity = "jaipur"
+
+accountEmail= "nishu@gmail.com"
+accountPassword = "12334445"
+accountCity = "lucknow"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all js code as newer version
+
+//alert( 3 + 3) // we are using nodejs, not browser
+
+console.log("Arvind")
+
+let name = "Arvind"
+let age = 21
+let isLoggedIn = false
+
+console.log(typeof "Arvind");
